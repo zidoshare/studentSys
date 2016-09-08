e3194bdbdb0fec719062810239aa3c5e4d9bfbc7
wuhongxu <1102612827@qq.com>
2016-09-08 17:41:48 +0800

    Revert "删除目录studentSys/src/main/java/com/hudongwx/studentSys"
    This reverts commit c3a2df4fec8419d20910446566f0ea9a5e611d18.

---
+++ studentSys/src/main/java/com/hudongwx/studentSys/service/MappingService.java
@@ -0,0 +1,76 @@
+package com.hudongwx.studentsys.service;
+
+import com.hudongwx.studentsys.common.Service;
+import com.hudongwx.studentsys.exceptions.ServiceException;
+import com.hudongwx.studentsys.model.Mapping;
+import com.hudongwx.studentsys.util.Common;
+import com.hudongwx.studentsys.util.StrPlusKit;
+import com.jfinal.log.Log;
+import com.jfinal.plugin.ehcache.CacheKit;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by wuhongxu on 2016/9/2 0002.
+ */
+public class MappingService extends Service {
+    private Log log = Log.getLog(getClass());
+    public List<Mapping> getMappings(){
+        return Mapping.dao.findByCache(Common.CACHE_FOEVER_LABEL, "mappingTree", "select * from stumanager_mapping");
+    }
+    public boolean _saveMappings(List<Mapping> mappings) {
+        try {
+            packingMappings(mappings);
+
+            //神玩法....以前从来不知道有这种用法...真神奇
+            mappings.forEach(Mapping::save);
+
+            List<Mapping> mappingTree = CacheKit.get(Common.CACHE_FOEVER_LABEL, "mappingTree");
+            if(null == mappingTree)
+                mappingTree = new ArrayList<>();
+            mappingTree.addAll(mappings);
+            CacheKit.put(Common.CACHE_FOEVER_LABEL,"mappingTree",mappingTree);
+            return true;
+        } catch (ServiceException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+    public void _saveMapping(Mapping mappingNode){
+        try {
+            packingMapping(mappingNode);
+            mappingNode.save();
+            List<Mapping> mappingTree = CacheKit.get(Common.CACHE_FOEVER_LABEL, "mappingTree");
+            mappingTree.add(mappingNode);
+            CacheKit.put(Common.CACHE_FOEVER_LABEL,"mappingTree",mappingTree);
+        } catch (ServiceException e) {
+            e.printStackTrace();
+        }
+
+    }
+    public void _deleteMapping(Mapping mapping){
+        mapping.delete();
+        List<Mapping> mappingTree = CacheKit.get(Common.CACHE_FOEVER_LABEL, "mappingTree");
+        mappingTree.remove(mapping);
+        CacheKit.put(Common.CACHE_FOEVER_LABEL,"mappingTree",mappingTree);
+    }
+    public void _updateMapping(Mapping mapping){
+        mapping.update();
+    }
+    public void packingMapping(Mapping mapping) throws ServiceException {
+        if(StrPlusKit.isEmpty(mapping.getTitle()))
+            throw new ServiceException("mapping's title cannot be null or \"\"");
+        if(StrPlusKit.isEmpty(mapping.getUrl()))
+            throw new ServiceException("mapping's url cannot be null or \"\"");
+        if(mapping.getId() == null || mapping.getId() == 0)
+            mapping.setId(System.currentTimeMillis());
+    }
+    public void packingMappings(List<Mapping> mappings) throws ServiceException {
+        for(Mapping mapping : mappings){
+            packingMapping(mapping);
+        }
+    }
+
+
+}
---
+++ studentSys/src/main/java/com/hudongwx/studentSys/util/RenderKit.java
@@ -0,0 +1,35 @@
+package com.hudongwx.studentsys.util;
+
+import com.alibaba.fastjson.JSONObject;
+import com.jfinal.core.Controller;
+
+/**
+ * Created by wuhongxu on 2016/8/31 0031.
+ */
+public class RenderKit {
+    public static void renderSuccess(Controller me){
+        renderState(me,"success");
+    }
+    public static void renderSuccess(Controller me,String msg){
+        renderMsg(me,msg,"success");
+    }
+    public static void renderError(Controller me){
+        renderState(me,"error");
+    }
+    public static void renderError(Controller me,String msg){
+        renderMsg(me,msg,"success");
+    }
+    public static void renderState(Controller me,String state){
+        renderMsg(me,"",state);
+    }
+    public static void renderMsg(Controller me,String msg,String state){
+        renderMsgWidthLocation(me,msg,state,"");
+    }
+    public static void renderMsgWidthLocation(Controller me,String msg,String state,String url){
+        JSONObject obj = new JSONObject();
+        obj.put("state",state);
+        obj.put("msg",msg);
+        obj.put("url",url);
+        me.renderJson(obj);
+    }
+}
